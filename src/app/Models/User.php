e735c5eb550d67100e0c87e0fb56e28f60ba3b79
HirokiYumoto <zyarippa6@gmail.com>
2026-02-03 13:36:55 +0900

    ログイン新規登録画面のデザイン変更

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,11 +10,6 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -23,28 +17,15 @@ class User extends Authenticatable
         'role_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 
     // ユーザーがお気に入り登録したデータ
     public function favorites()
@@ -58,7 +39,7 @@ class User extends Authenticatable
         return $this->hasMany(Review::class);
     }
 
-    // ★★★ 追加：ユーザー自身が所有（作成）した店舗データ ★★★
+    // ユーザー自身が所有（作成）した店舗データ
     public function restaurants()
     {
         return $this->hasMany(Restaurant::class);
